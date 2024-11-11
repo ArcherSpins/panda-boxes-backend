14a8dc85df9454189fb81676cc777d6fa5508d99
misapanda <misapanda@proton.me>
2024-11-11 15:56:00 +0900

    update database connect

--- db/database.go
+++ db/database.go
@@ -1,28 +1,7 @@
-// package db
-
-// import (
-// 	"log"
-// 	"panda-boxes/configs"
-
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func ConnectDatabase(config *configs.Config) {
-// 	dsn := config.GetDSN()
-// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
-// 	}
-// 	DB = db
-// 	log.Println("Соединение с базой данных установлено")
-// }
-
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -33,14 +12,27 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Fatal("DATABASE_URL не установлена")
+	// Собираем DSN из переменных окружения
+	host := os.Getenv("PGHOST")
+	port := os.Getenv("PGPORT")
+	user := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+	dbname := os.Getenv("PGDATABASE")
+
+	// Проверка, что переменные окружения установлены
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
+		log.Fatal("Не все переменные окружения для базы данных установлены")
 	}
+
+	// Создаем DSN строку с sslmode=require
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		host, port, user, password, dbname)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
 	}
+
 	DB = db
 	log.Println("Соединение с базой данных установлено")
 }
